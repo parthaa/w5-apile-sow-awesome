3922e318119bb58656d366fdbdb2b5a3a9689248
Partha Aji <paji@redhat.com>
2018-11-27 01:08:19 -0500

    Initial django commit

---
+++ mynews/posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+class Post(models.Model):
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+  link = models.TextField(null=True, blank=True)
+  title = models.TextField(null=True, blank=True)
+  description = models.TextField(null=True, blank=True)
+
+class Vote(models.Model):
+  voter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="votes")
+  post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name="votes")
+
+  class Meta:
+    unique_together= (('post', 'voter'),)
+
+class Comment(models.Model):
+  description = models.TextField(null=True, blank=True)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+  post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name="comments")
+
+  class Meta:
+    unique_together= (('post', 'author'),)
