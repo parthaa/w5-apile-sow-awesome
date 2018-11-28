5a96449c0f5b12adee5d93dbcc90c88254ca6ff9
Ardra Salemy <ardrasalemy@gmail.com>
2018-11-28 16:12:03 -0500

    Add to urls.py

--- linkme/linkme/urls.py
+++ linkme/linkme/urls.py
@@ -18,25 +18,30 @@ from django.urls import path, include
 from linkworld import views
 # from linkworld.backends import MyRegistrationView
 from django.contrib.auth.views import (
-    # PasswordChangeView,
+    PasswordChangeView,
     # PasswordChangeDoneView,
     PasswordResetView,
-    PasswordResetDoneView,
+    # PasswordResetDoneView,
     PasswordResetConfirmView,
     PasswordResetCompleteView,
 )
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path('accounts/password/reset/', PasswordResetView,
-         {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
-    path('accounts/password/reset/done/', PasswordResetDoneView,
-         {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
-    path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView,
-         {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
-    path('accounts/password/done/', PasswordResetCompleteView,
-         {'template_name': 'registration/password_reset_complete.html'},
+    path('accounts/password/reset/', PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'), name="password_reset"),
+    path('accounts/password/change/', PasswordChangeView.as_view(
+        template_name='registration/password_change_form.html'), name="password_change"),
+    # path('accounts/password/change/done/', PasswordChangeDoneView.as_view(
+    #     template_name='registration/password_change_done.html'), name="password_change_done"),
+    # path('accounts/password/reset/done/', PasswordResetDoneView.as_view(
+    #     template_name='registration/password_reset_done.html'), name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('accounts/password/done/', PasswordResetCompleteView.as_view,
+         template_name='registration/password_reset_complete.html'),
          name="password_reset_complete"),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('posts/<slug>/')
     path('admin/', admin.site.urls),
 ]
