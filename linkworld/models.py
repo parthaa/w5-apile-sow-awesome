3df4a13268789817d4ffb121131b9accf10f9fd0
Ardra Salemy <ardrasalemy@gmail.com>
2018-11-29 19:41:24 -0500

    Merges -b registration with master, which addes reg and password files. Picks most recent models.py and deletes extra models.py and extra linkworld directory.

--- linkworld/models.py
+++ linkworld/models.py
@@ -6,7 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     title = models.CharField(max_length=255)
-    url = models.URLField()
+    url = models.URLField(null=True)
+    date = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
@@ -20,3 +21,26 @@ class Vote(models.Model):
     post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     choice = models.BooleanField(default=False)
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+
+# class Post(models.Model):
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     title = models.CharField(max_length=255)
+#     url = models.URLField()
+
+
+# class Comment(models.Model):
+#     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+
+
+# class Vote(models.Model):
+
+#     post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     choice = models.BooleanField(default=False)
